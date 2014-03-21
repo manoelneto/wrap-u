3ec10665f875c93e38019f307be5ed00e629a6c0
Manoel Quirino Neto <manoelifpb@gmail.com>
2014-03-21 13:09:46 -0300

    retirando unidades

--- U.py
+++ U.py
@@ -7,6 +7,7 @@ class UCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         sels = self.view.sel()
         edit = self.view.begin_edit()
+        integ_re = re.compile(r'\d+')
         u_re = re.compile(r'^u\(([^\)]+)\)$')
 
         for sel in sels:
@@ -15,6 +16,9 @@ class UCommand(sublime_plugin.TextCommand):
             if has_u:
                 new_content = has_u.group(1)
             else:
+                content = ' '.join(
+                    [integ_re.search(x).group(0) for x in content.split(' ')]
+                )
                 new_content = 'u(%s)' % (content,)
             self.view.replace(edit, sel, new_content)
         self.view.end_edit(edit)

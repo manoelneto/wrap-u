4de6d67b5f76bb233aefd8d100e0b75ec9187970
Manoel Quirino Neto <manoelifpb@gmail.com>
2014-03-21 13:11:35 -0300

    retirando unidades - colocando um try

--- U.py
+++ U.py
@@ -16,9 +16,13 @@ class UCommand(sublime_plugin.TextCommand):
             if has_u:
                 new_content = has_u.group(1)
             else:
-                content = ' '.join(
-                    [integ_re.search(x).group(0) for x in content.split(' ')]
-                )
+                try:
+                    content = ' '.join(
+                        [integ_re.search(x).group(0)
+                            for x in content.split(' ')]
+                    )
+                except:
+                    pass
                 new_content = 'u(%s)' % (content,)
             self.view.replace(edit, sel, new_content)
         self.view.end_edit(edit)
